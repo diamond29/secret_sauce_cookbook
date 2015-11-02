ff1245572e341ca1f3a080d2cefe0f76a1dd4e3c
Greg Diamond <gregdiamond29@gmail.com>
2015-11-01 20:17:14 -0500

    added something simple that just checks out my dotfiles

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+default['dotfiles_repo'] = 'http://github.com/diamond29/dotfiles.git'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+git "#{ENV['HOME']}/dotfiles" do
+  repository node['dotfiles_repo']
+  revision 'master'
+end
